f50a5617389436592664d3ce748adc114c9eb779
Justin Dority <justindority@gmail.com>
2022-10-06 11:56:23 -0500

    initial commit

---
+++ models/orders.py
@@ -0,0 +1,7 @@
+class Order():
+    def __init__(self, id, metalId, sizeId, styleId, timestamp):
+        self.id = id
+        self.metalId = metalId
+        self.sizeId = sizeId
+        self.styleId = styleId
+        self.timestamp: timestamp
---
+++ models/sizes.py
@@ -0,0 +1,5 @@
+class Size():
+    def __init__(self, id, carets, price):
+        self.id = id
+        self.carets = carets
+        self.price = price
